a0f1531f1bd477e2e498146956a008d3e7107a23
Sam Pram <aldindzikri@gmail.com>
2024-09-26 12:34:02 +0700

    feat: setup directory and file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmployeeModule } from './models/employee/employee.module';
+import { PostgreModule } from './providers/database/postgre/postgre.module';
 
 @Module({
-  imports: [],
+  imports: [EmployeeModule, PostgreModule],
   controllers: [AppController],
   providers: [AppService],
 })
